50e9864f27a6a44a0fa459b2d47b22ff695d4df5
Ivan Giuliani <giuliani.v@gmail.com>
2013-07-17 22:54:03 +0200

    Added another problem

---
+++ interviewquestions/java/CharDuplicates.java
@@ -0,0 +1,37 @@
+package interviewquestions.java;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Remove duplicate characters in a given string keeping only the first occurrence.
+ * For example, if the input is "tree traversal" the output will be "tre avsl".
+ */
+public class CharDuplicates {
+  public static String remdup(String input) {
+    Set<Character> characterSet = new HashSet<Character>();
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < input.length(); i++) {
+      char ch = input.charAt(i);
+      if (!characterSet.contains(ch)) {
+        characterSet.add(ch);
+        builder.append(ch);
+      }
+    }
+    return builder.toString();
+  }
+
+  public static void main(String[] args) {
+    test(remdup("stringstring").equals("string"));
+    test(remdup("tree traversal").equals("tre avsl"));
+    test(remdup("no duplicates").equals("no duplicates"));
+    test(remdup("123456123451234").equals("123456"));
+  }
+
+  public static void test(boolean condition) {
+    // assertions are disabled by default in java, mimic their behaviour here
+    if (!condition) {
+      throw new AssertionError("invalid test");
+    }
+  }
+}
