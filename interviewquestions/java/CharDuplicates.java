22a8c94395386a09f0272d39bf2455f8e58ba09c
Ivan Giuliani <giuliani.v@gmail.com>
2013-08-15 12:12:41 +0200

    Added alternative solution to character duplicate removal question

--- interviewquestions/java/CharDuplicates.java
+++ interviewquestions/java/CharDuplicates.java
@@ -21,11 +21,44 @@ public class CharDuplicates {
     return builder.toString();
   }
 
+  /*
+   * The same as above, but without using additional data structures
+   * We iterate for every character over every other previous character
+   * to check if we already encountered it, leading to an O(n^2) solution
+   */
+  public static String remdupNoDS(String input) {
+    StringBuilder builder = new StringBuilder();
+    boolean encountered;
+    char ch;
+
+    for (int i = 0; i < input.length(); i++) {
+      ch = input.charAt(i);
+      encountered = false;
+
+      for (int j = 0; j < i && !encountered; j++) {
+        if (ch == input.charAt(j)) {
+          encountered = true;
+        }
+      }
+
+      if (!encountered) builder.append(ch);
+    }
+
+    return builder.toString();
+  }
+
   public static void main(String[] args) {
     test(remdup("stringstring").equals("string"));
     test(remdup("tree traversal").equals("tre avsl"));
     test(remdup("no duplicates").equals("no duplicates"));
     test(remdup("123456123451234").equals("123456"));
+    test(remdup("").equals(""));
+
+    test(remdupNoDS("stringstring").equals("string"));
+    test(remdupNoDS("tree traversal").equals("tre avsl"));
+    test(remdupNoDS("no duplicates").equals("no duplicates"));
+    test(remdupNoDS("123456123451234").equals("123456"));
+    test(remdupNoDS("").equals(""));
   }
 
   public static void test(boolean condition) {
