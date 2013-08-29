803df7ae35db70e6d871f66c8159a44f78632f8a
Ivan Giuliani <giuliani.v@gmail.com>
2013-08-29 13:40:12 +0200

    It turns out, c++ implementation of the heap is incomplete

--- cpp/trees/heap.h
+++ cpp/trees/heap.h
@@ -19,33 +19,33 @@ namespace ds {
 			T *array;
 			unsigned int length;
 			unsigned int n_ariety;
-	
+
 		public:
 			/* Create a 'n_ariety' tree  which will hold at most 'size' nodes */
 			Heap(unsigned int size, unsigned int n_ariety) {
 				this->array = new T[size];
 				this->n_ariety = n_ariety;
 			};
-	
+
 			~Heap() {
 				delete[] this->array;
 			};
-	
+
 			T* at(unsigned int position) {
 				return &this->array[position];
 			};
-	
+
 			T &operator[] (unsigned int p) { return this->array[p]; };
-	
+
 			/*******************
 			 * support methods *
 			 *******************/
-	
+
 			/* Obtains the parent of the specified item */
 			unsigned int parent(unsigned int pos) {
 				return floor((1/(float)this->n_ariety) * (pos - 1));
 			};
-	
+
 			/* Obtains the i-th child of the item at position 'pos' */
 			unsigned int ith_child(unsigned int pos, unsigned int i) {
 				return (this->n_ariety * pos) + i;
@@ -53,5 +53,5 @@ namespace ds {
 	};
 
 } /* namespace:ds */
-	
+
 #endif /* HEAP_H */
--- cpp/trees/trie.h
+++ cpp/trees/trie.h
@@ -20,18 +20,18 @@ namespace ds {
 				for (unsigned short int i = 0; i < key.length(); i++)
 					if !(key[i] >= 'a' && key[i] <= 'z') throw TrieInvalidKey();
 			};
-	
+
 		public:
 			Trie *ptrs[26];
 			bool leaf;
 			T value;
-	
+
 			Trie() {
 				this->leaf = false;
 				for (unsigned short int i = 0; i < 26; i++)
 					ptrs[i] = NULL;
 			};
-	
+
 			~Trie() {
 				for (unsigned short int i = 0; i < 26; i++) {
 					if (this->ptrs[i]) delete this->ptrs[i];
@@ -46,14 +46,14 @@ namespace ds {
 					this->leaf = true;
 					return;
 				}
-	
+
 				unsigned short int pos = key[0] - 'a';
 				if (!this->ptrs[pos])
 					this->ptrs[pos] = new Trie();
-	
+
 				this->ptrs[pos]->insert(key.substr(1), value);
 			};
-	
+
 			Trie *find(string key) {
 				assume_valid_key(key);
 
@@ -64,16 +64,16 @@ namespace ds {
 					curr = curr->ptrs[pos];
 					if (!curr) return NULL;
 				}
-	
+
 				return curr;
 			};
-	
+
 			T get(string key) {
 				assume_valid_key(key);
 
 				Trie *ptr = this->find(key);
 				if (!ptr) throw TrieNotFound();
-	
+
 				if (ptr->leaf)
 					return ptr->value;
 				throw TrieNotFound();
