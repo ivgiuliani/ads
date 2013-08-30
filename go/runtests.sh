e38f5498583447a9765515e9d52a349eaa86d7c9
Ivan Giuliani <giuliani.v@gmail.com>
2013-08-30 18:25:30 +0200

    Allow to execute only golang tests for the specified modules

--- go/runtests.sh
+++ go/runtests.sh
@@ -7,8 +7,14 @@ MODULES=(
 
 export GOPATH=(pwd)
 
+if [ "$#" -eq 0 ]; then
+  TEST_MODULES=${MODULES[@]}
+else
+  TEST_MODULES=( $@ )
+fi
+
 pushd src/ads > /dev/null
-for module in ${MODULES[@]}
+for module in ${TEST_MODULES[@]}
 do
   echo "--------------------------------------------------"
   echo "Module: $module"
