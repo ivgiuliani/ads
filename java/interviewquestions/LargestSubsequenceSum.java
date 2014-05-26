293baddd0346203ba60f0c712afbf00e6285caf9
Ivan Giuliani <giuliani.v@gmail.com>
2014-05-26 19:10:03 +0200

    Ported interview question to new style testcase

--- java/interviewquestions/LargestSubsequenceSum.java
+++ java/interviewquestions/LargestSubsequenceSum.java
@@ -7,7 +7,7 @@ package interviewquestions;
  * Input: {2, -8, 3, -2, 4, -10}
  * Output: 5 (i.e., {3, -2, 4} )
  */
-public class LargestSubsequenceSum {
+public class LargestSubsequenceSum extends TestCase {
   public static class Subsequence {
     public int start;
     public int stop;
@@ -46,23 +46,16 @@ public class LargestSubsequenceSum {
   }
 
   public static void main(String[] args) {
-    test(largestSubsequenceSum(new int[] {2, -8, 3, -2, 4, -10}).sum == 5);
-    test(largestSubsequenceSum(new int[] {2, -8, 3, -2, 4, -10}).start == 2);
-    test(largestSubsequenceSum(new int[] {2, -8, 3, -2, 4, -10}).stop == 4);
+    assertEquals(5, largestSubsequenceSum(new int[] {2, -8, 3, -2, 4, -10}).sum);
+    assertEquals(2, largestSubsequenceSum(new int[] {2, -8, 3, -2, 4, -10}).start);
+    assertEquals(4, largestSubsequenceSum(new int[] {2, -8, 3, -2, 4, -10}).stop);
 
-    test(largestSubsequenceSum(new int[] {-1, -2, -3, -4, -5, -6}).sum == 0);
-    test(largestSubsequenceSum(new int[] {-1, -2, -3, -4, -5, -6}).start == -1);
-    test(largestSubsequenceSum(new int[] {-1, -2, -3, -4, -5, -6}).stop == -1);
+    assertEquals(0, largestSubsequenceSum(new int[] {-1, -2, -3, -4, -5, -6}).sum);
+    assertEquals(-1, largestSubsequenceSum(new int[] {-1, -2, -3, -4, -5, -6}).start);
+    assertEquals(-1, largestSubsequenceSum(new int[] {-1, -2, -3, -4, -5, -6}).stop);
 
-    test(largestSubsequenceSum(new int[] {1, -2, -3, -4, -5, -6}).sum == 1);
-    test(largestSubsequenceSum(new int[] {1, -2, -3, -4, -5, -6}).start == 0);
-    test(largestSubsequenceSum(new int[] {1, -2, -3, -4, -5, -6}).stop == 0);
-  }
-
-  public static void test(boolean condition) {
-    // assertions are disabled by default in java, mimic their behaviour here
-    if (!condition) {
-      throw new AssertionError("invalid test");
-    }
+    assertEquals(1, largestSubsequenceSum(new int[] {1, -2, -3, -4, -5, -6}).sum);
+    assertEquals(0, largestSubsequenceSum(new int[] {1, -2, -3, -4, -5, -6}).start);
+    assertEquals(0, largestSubsequenceSum(new int[] {1, -2, -3, -4, -5, -6}).stop);
   }
 }
