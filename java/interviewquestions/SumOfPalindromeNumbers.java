b28f2441c5e4aa642e611f9becfe683ab2ce2a7b
Ivan Giuliani <giuliani.v@gmail.com>
2014-05-29 11:47:45 +0200

    Ported SumOfPalindromeNumbers to new testcase ABC

--- java/interviewquestions/SumOfPalindromeNumbers.java
+++ java/interviewquestions/SumOfPalindromeNumbers.java
@@ -10,13 +10,13 @@ package interviewquestions;
  *   1473 + 3741 = 5214
  *   5214 + 4125 = 9339 (palindrome)
  *
- *   In this particular case the palindrome 9339 appeared after the 4th addition.
- *   This method leads to palindromes in a few step for almost all of the integers.
- *   But there are interesting exceptions. 196 is the first number for which no
- *   palindrome has been found. It is not proven though, that there is no
- *   such a palindrome.
+ * In this particular case the palindrome 9339 appeared after the 4th addition.
+ * This method leads to palindromes in a few step for almost all of the integers.
+ * But there are interesting exceptions. 196 is the first number for which no
+ * palindrome has been found. It is not proven though, that there is no
+ * such a palindrome.
  */
-public class SumOfPalindromeNumbers {
+public class SumOfPalindromeNumbers extends TestCase {
   public static class Result {
     public int iterations;
     public int value;
@@ -43,22 +43,15 @@ public class SumOfPalindromeNumbers {
   }
 
   public static void main(String[] args) {
-    test(palindromeSum(195).iterations == 4);
-    test(palindromeSum(195).value == 9339);
-    test(palindromeSum(12345).iterations == 1);
-    test(palindromeSum(12345).value == 66666);
-    test(palindromeSum(44481).iterations == 9);
-    test(palindromeSum(44481).value == 144222441);
-    test(palindromeSum(25).iterations == 1);
-    test(palindromeSum(25).value == 77);
-    test(palindromeSum(99).iterations == 0);
-    test(palindromeSum(99).value == 99);
-  }
-
-  public static void test(boolean condition) {
-    // assertions are disabled by default in java, mimic their behaviour here
-    if (!condition) {
-      throw new AssertionError("invalid test");
-    }
+    assertEquals(4, palindromeSum(195).iterations);
+    assertEquals(9339, palindromeSum(195).value);
+    assertEquals(1, palindromeSum(12345).iterations);
+    assertEquals(66666, palindromeSum(12345).value);
+    assertEquals(9, palindromeSum(44481).iterations);
+    assertEquals(144222441, palindromeSum(44481).value);
+    assertEquals(1, palindromeSum(25).iterations);
+    assertEquals(77, palindromeSum(25).value);
+    assertEquals(0, palindromeSum(99).iterations);
+    assertEquals(99, palindromeSum(99).value);
   }
 }
