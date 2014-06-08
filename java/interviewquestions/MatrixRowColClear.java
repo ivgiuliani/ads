45aece69de0d7be3c7876f42289de6213291804b
Ivan Giuliani <giuliani.v@gmail.com>
2014-06-08 18:33:02 +0200

    Ported interview question to new testcase style

--- java/interviewquestions/MatrixRowColClear.java
+++ java/interviewquestions/MatrixRowColClear.java
@@ -4,7 +4,7 @@ package interviewquestions;
  * Write an algorithm such that if an element in an MxN matrix is 0,
  * its entire row and column is set to 0.
  */
-public class MatrixRowColClear {
+public class MatrixRowColClear extends TestCase {
   public static void clear(int[][] matrix, int rows, int cols) {
     int resetRows[] = new int[rows];
     int resetCols[] = new int[cols];
@@ -35,35 +35,28 @@ public class MatrixRowColClear {
 
   public static void main(String[] args) {
     int[][] matrix = new int[][] {
-        { 1, 2, 3, 4},
-        { 2, 3, 4, 5},
-        { 3, 4, 5, 6},
-        { 4, 5, 6, 0}
+        { 1, 2, 3, 4 },
+        { 2, 3, 4, 5 },
+        { 3, 4, 5, 6 },
+        { 4, 5, 6, 0 }
     };
 
     clear(matrix, 4, 4);
-    test(matrix[0][0] == 1);
-    test(matrix[0][1] == 2);
-    test(matrix[0][2] == 3);
-    test(matrix[0][3] == 0);
-    test(matrix[1][0] == 2);
-    test(matrix[1][1] == 3);
-    test(matrix[1][2] == 4);
-    test(matrix[1][3] == 0);
-    test(matrix[2][0] == 3);
-    test(matrix[2][1] == 4);
-    test(matrix[2][2] == 5);
-    test(matrix[2][3] == 0);
-    test(matrix[3][0] == 0);
-    test(matrix[3][1] == 0);
-    test(matrix[3][2] == 0);
-    test(matrix[3][3] == 0);
-  }
-
-  public static void test(boolean condition) {
-    // assertions are disabled by default in java, mimic their behaviour here
-    if (!condition) {
-      throw new AssertionError("invalid test");
-    }
+    assertEquals(1, matrix[0][0]);
+    assertEquals(2, matrix[0][1]);
+    assertEquals(3, matrix[0][2]);
+    assertEquals(0, matrix[0][3]);
+    assertEquals(2, matrix[1][0]);
+    assertEquals(3, matrix[1][1]);
+    assertEquals(4, matrix[1][2]);
+    assertEquals(0, matrix[1][3]);
+    assertEquals(3, matrix[2][0]);
+    assertEquals(4, matrix[2][1]);
+    assertEquals(5, matrix[2][2]);
+    assertEquals(0, matrix[2][3]);
+    assertEquals(0, matrix[3][0]);
+    assertEquals(0, matrix[3][1]);
+    assertEquals(0, matrix[3][2]);
+    assertEquals(0, matrix[3][3]);
   }
 }
