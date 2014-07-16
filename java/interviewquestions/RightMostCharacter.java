811cedbc61e79f17a0ce93a35d52008f7e236911
Ivan Giuliani <giuliani.v@gmail.com>
2014-07-16 20:04:05 +0200

    Ported interview question to new testcase style

--- java/interviewquestions/RightMostCharacter.java
+++ java/interviewquestions/RightMostCharacter.java
@@ -4,7 +4,7 @@ package interviewquestions;
  * Given a string 'S' and a character 't'. Print out the position of the rightmost
  * occurrence of 't' (case matters) in 'S' or -1 if there is none.
  */
-public class RightMostCharacter {
+public class RightMostCharacter extends TestCase {
   public static int rightmost(String string, Character ch) {
     for (int idx = string.length() - 1; idx >= 0; --idx) {
       if (string.charAt(idx) == ch) return idx;
@@ -13,15 +13,15 @@ public class RightMostCharacter {
   }
 
   public static void main(String[] args) {
-    test(rightmost("Hello World", 'r') == 8);
-    test(rightmost("Hello World", 'd') == 10);
-    test(rightmost("Hello WOrld", 'o') == 4);
-    test(rightmost("Hello WOrld", 'O') == 7);
-    test(rightmost("aaaaaaaaaaa", 'a') == 10);
-    test(rightmost("", 'a') == -1);
-    test(rightmost("How to learn to Cook", 'C') == 16);
-    test(rightmost("What is your name", 'm') == 15);
-    test(rightmost("lKHFLOpCmu0bxE0AooKQ8gYcrvf7Go22pMVKjFCc7DU9qIpcd55 Px00SdUCV4fzSq", 'W') == -1);
+    assertEquals(8, rightmost("Hello World", 'r'));
+    assertEquals(10, rightmost("Hello World", 'd'));
+    assertEquals(4, rightmost("Hello WOrld", 'o'));
+    assertEquals(7, rightmost("Hello WOrld", 'O'));
+    assertEquals(10, rightmost("aaaaaaaaaaa", 'a'));
+    assertEquals(-1, rightmost("", 'a'));
+    assertEquals(16, rightmost("How to learn to Cook", 'C'));
+    assertEquals(15, rightmost("What is your name", 'm'));
+    assertEquals(-1, rightmost("lKHFLOpCmu0bxE0AooKQ8gYcrvf7Go22pMVKjFCc7DU9qIpcd55 Px00SdUCV4fzSq", 'W'));
   }
 
   public static void test(boolean condition) {
