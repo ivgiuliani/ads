1f10138eaaa781cc4a2f663d4326a14268de8585
Ivan Giuliani <giuliani.v@gmail.com>
2014-07-30 14:14:22 +0200

    Added interview question

---
+++ python/interviewquestions/is_string_rotated.py
@@ -0,0 +1,28 @@
+"""
+Write a code to test whether string s2 is obtained by rotating the string s1
+to the left by exactly 2 places.
+
+Example:
+  S1="amazon" S2="azonam" return true
+  S1="quality" S2="lityqua" return false
+"""
+
+import unittest
+
+
+def is_string_rotated(s1, s2, n=2):
+    l1, l2 = len(s1), len(s2)
+    if l1 != l2:
+        return False
+
+    for i in range(l1):
+        if s1[i] != s2[(i - n) % l1]:
+            return False
+
+    return True
+
+
+class IsStringRotatedTest(unittest.TestCase):
+    def test_is_string_rotated(self):
+        self.assertTrue(is_string_rotated("amazon", "azonam"))
+        self.assertFalse(is_string_rotated("quality", "lityqua"))
